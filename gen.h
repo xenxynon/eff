737ed3121bb5811928ecfbd20915199d4a2629b1
xenxynon <flynryder427@gmail.com>
2025-03-14 19:02:06 +0000

    eqs

---
+++ gen.h
@@ -0,0 +1,314 @@
+ConfigHeader
+field: version, value: 4, offset: 0x0, offset32: 0x0, 
+field: size, value: 128, offset: 0x4, offset32: 0x1, 
+field: unknown0, value: 0.0, offset: 0x8, offset32: 0x2, 
+field: client, value: b'NT', offset: 0x10, offset32: 0x4, 
+field: client_item, value: b'X', offset: 0x24, offset32: 0x9, 
+field: device_type, value: b'X', offset: 0x38, offset32: 0xe, 
+field: vibrator_type, value: b'ELA0809', offset: 0x4c, offset32: 0x13, 
+
+ConfigParamsV1
+field: unknown0, value: 0.9, offset: 0x60, offset32: 0x18, 
+field: unknown1, value: 0.0, offset: 0x68, offset32: 0x1a, 
+field: unknown2, value: 170.0, offset: 0x70, offset32: 0x1c, 
+field: unknown3, value: 176.0, offset: 0x78, offset32: 0x1e, 
+field: unknown4, value: 0.00055, offset: 0x80, offset32: 0x20, 
+field: unknown5, value: 8.27, offset: 0x88, offset32: 0x22, 
+field: unknown6, value: 6.8e-05, offset: 0x90, offset32: 0x24, 
+field: unknown7, value: 0.266, offset: 0x98, offset32: 0x26, 
+field: unknown8, value: 0.00125, offset: 0xa0, offset32: 0x28, 
+field: unknown9, value: 1535.0, offset: 0xa8, offset32: 0x2a, 
+field: unknown10, value: 0.055, offset: 0xb0, offset32: 0x2c, 
+
+ConfigParamsV2
+field: interrupt_protect_time, value: 30.0, offset: 0xb8, offset32: 0x2e, 
+
+ConfigParamsV3
+field: unknown1, value: 1.0, offset: 0xc0, offset32: 0x30, 
+
+ConfigParamsV4
+field: jnd_value, value: 0.14, offset: 0xc8, offset32: 0x32, 
+field: jnd_step, value: 10.0, offset: 0xd0, offset32: 0x34, 
+
+Leftover config header data:
+bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00')
+
+ConfigMore
+field: unknown0, value: 21110401, offset: 0xe0, offset32: 0x38, 
+field: size, value: 1608, offset: 0xe4, offset32: 0x39, 
+field: length, value: 0, offset: 0xe8, offset32: 0x3a, 
+field: unknown1, value: 0, offset: 0xec, offset32: 0x3b, 
+
+ConfigMore
+field: unknown0, value: 21110401, offset: 0x738, offset32: 0x1ce, 
+field: size, value: 3608, offset: 0x73c, offset32: 0x1cf, 
+field: length, value: 0, offset: 0x740, offset32: 0x1d0, 
+field: unknown1, value: 0, offset: 0x744, offset32: 0x1d1, 
+
+ConfigEffects
+field: unknown0, value: 21110401, offset: 0x0, offset32: 0x0, 
+field: size, value: 3, offset: 0x4, offset32: 0x1, 
+field: unknown1, value: 0, offset: 0x8, offset32: 0x2, 
+
+ConfigEffect
+field: id, value: 1, offset: 0xc, offset32: 0x3, 
+field: effect_level, value: 3, offset: 0x10, offset32: 0x4, 
+field: size, value: 260, offset: 0x14, offset32: 0x5, 
+field: unknown0, value: 0, offset: 0x18, offset32: 0x6, 
+
+ConfigEffect
+field: id, value: 1, offset: 0x120, offset32: 0x48, 
+field: effect_level, value: 1, offset: 0x124, offset32: 0x49, 
+field: size, value: 235, offset: 0x128, offset32: 0x4a, 
+field: unknown0, value: 0, offset: 0x12c, offset32: 0x4b, 
+
+ConfigEffect
+field: id, value: 1, offset: 0x21b, 
+field: effect_level, value: 2, offset: 0x21f, 
+field: size, value: 253, offset: 0x223, 
+field: unknown0, value: 0, offset: 0x227, 
+
+ConfigEffect
+field: id, value: 2, offset: 0x328, offset32: 0xca, 
+field: effect_level, value: 3, offset: 0x32c, offset32: 0xcb, 
+field: size, value: 1990, offset: 0x330, offset32: 0xcc, 
+field: unknown0, value: 0, offset: 0x334, offset32: 0xcd, 
+
+ConfigEffect
+field: id, value: 2, offset: 0xafe, 
+field: effect_level, value: 1, offset: 0xb02, 
+field: size, value: 1942, offset: 0xb06, 
+field: unknown0, value: 0, offset: 0xb0a, 
+
+ConfigEffect
+field: id, value: 2, offset: 0x12a4, offset32: 0x4a9, 
+field: effect_level, value: 2, offset: 0x12a8, offset32: 0x4aa, 
+field: size, value: 1974, offset: 0x12ac, offset32: 0x4ab, 
+field: unknown0, value: 0, offset: 0x12b0, offset32: 0x4ac, 
+
+ConfigEffect
+field: id, value: 3, offset: 0x1a6a, 
+field: effect_level, value: 3, offset: 0x1a6e, 
+field: size, value: 674, offset: 0x1a72, 
+field: unknown0, value: 0, offset: 0x1a76, 
+
+ConfigEffect
+field: id, value: 3, offset: 0x1d1c, offset32: 0x747, 
+field: effect_level, value: 1, offset: 0x1d20, offset32: 0x748, 
+field: size, value: 631, offset: 0x1d24, offset32: 0x749, 
+field: unknown0, value: 0, offset: 0x1d28, offset32: 0x74a, 
+
+ConfigEffect
+field: id, value: 3, offset: 0x1fa3, 
+field: effect_level, value: 2, offset: 0x1fa7, 
+field: size, value: 638, offset: 0x1fab, 
+field: unknown0, value: 0, offset: 0x1faf, 
+
+ConfigEffects
+field: unknown0, value: 21110401, offset: 0x0, offset32: 0x0, 
+field: size, value: 11, offset: 0x4, offset32: 0x1, 
+field: unknown1, value: 0, offset: 0x8, offset32: 0x2, 
+
+ConfigEffect
+field: id, value: 1, offset: 0xc, offset32: 0x3, 
+field: effect_level, value: 3, offset: 0x10, offset32: 0x4, 
+field: size, value: 529, offset: 0x14, offset32: 0x5, 
+field: unknown0, value: 0, offset: 0x18, offset32: 0x6, 
+
+ConfigEffect
+field: id, value: 1, offset: 0x22d, 
+field: effect_level, value: 1, offset: 0x231, 
+field: size, value: 519, offset: 0x235, 
+field: unknown0, value: 0, offset: 0x239, 
+
+ConfigEffect
+field: id, value: 1, offset: 0x444, offset32: 0x111, 
+field: effect_level, value: 2, offset: 0x448, offset32: 0x112, 
+field: size, value: 526, offset: 0x44c, offset32: 0x113, 
+field: unknown0, value: 0, offset: 0x450, offset32: 0x114, 
+
+ConfigEffect
+field: id, value: 2, offset: 0x662, 
+field: effect_level, value: 3, offset: 0x666, 
+field: size, value: 486, offset: 0x66a, 
+field: unknown0, value: 0, offset: 0x66e, 
+
+ConfigEffect
+field: id, value: 2, offset: 0x858, offset32: 0x216, 
+field: effect_level, value: 1, offset: 0x85c, offset32: 0x217, 
+field: size, value: 428, offset: 0x860, offset32: 0x218, 
+field: unknown0, value: 0, offset: 0x864, offset32: 0x219, 
+
+ConfigEffect
+field: id, value: 2, offset: 0xa14, offset32: 0x285, 
+field: effect_level, value: 2, offset: 0xa18, offset32: 0x286, 
+field: size, value: 430, offset: 0xa1c, offset32: 0x287, 
+field: unknown0, value: 0, offset: 0xa20, offset32: 0x288, 
+
+ConfigEffect
+field: id, value: 3, offset: 0xbd2, 
+field: effect_level, value: 3, offset: 0xbd6, 
+field: size, value: 486, offset: 0xbda, 
+field: unknown0, value: 0, offset: 0xbde, 
+
+ConfigEffect
+field: id, value: 3, offset: 0xdc8, offset32: 0x372, 
+field: effect_level, value: 1, offset: 0xdcc, offset32: 0x373, 
+field: size, value: 428, offset: 0xdd0, offset32: 0x374, 
+field: unknown0, value: 0, offset: 0xdd4, offset32: 0x375, 
+
+ConfigEffect
+field: id, value: 3, offset: 0xf84, offset32: 0x3e1, 
+field: effect_level, value: 2, offset: 0xf88, offset32: 0x3e2, 
+field: size, value: 430, offset: 0xf8c, offset32: 0x3e3, 
+field: unknown0, value: 0, offset: 0xf90, offset32: 0x3e4, 
+
+ConfigEffect
+field: id, value: 4, offset: 0x1142, 
+field: effect_level, value: 3, offset: 0x1146, 
+field: size, value: 486, offset: 0x114a, 
+field: unknown0, value: 0, offset: 0x114e, 
+
+ConfigEffect
+field: id, value: 4, offset: 0x1338, offset32: 0x4ce, 
+field: effect_level, value: 1, offset: 0x133c, offset32: 0x4cf, 
+field: size, value: 428, offset: 0x1340, offset32: 0x4d0, 
+field: unknown0, value: 0, offset: 0x1344, offset32: 0x4d1, 
+
+ConfigEffect
+field: id, value: 4, offset: 0x14f4, offset32: 0x53d, 
+field: effect_level, value: 2, offset: 0x14f8, offset32: 0x53e, 
+field: size, value: 430, offset: 0x14fc, offset32: 0x53f, 
+field: unknown0, value: 0, offset: 0x1500, offset32: 0x540, 
+
+ConfigEffect
+field: id, value: 5, offset: 0x16b2, 
+field: effect_level, value: 3, offset: 0x16b6, 
+field: size, value: 486, offset: 0x16ba, 
+field: unknown0, value: 0, offset: 0x16be, 
+
+ConfigEffect
+field: id, value: 5, offset: 0x18a8, offset32: 0x62a, 
+field: effect_level, value: 1, offset: 0x18ac, offset32: 0x62b, 
+field: size, value: 428, offset: 0x18b0, offset32: 0x62c, 
+field: unknown0, value: 0, offset: 0x18b4, offset32: 0x62d, 
+
+ConfigEffect
+field: id, value: 5, offset: 0x1a64, offset32: 0x699, 
+field: effect_level, value: 2, offset: 0x1a68, offset32: 0x69a, 
+field: size, value: 430, offset: 0x1a6c, offset32: 0x69b, 
+field: unknown0, value: 0, offset: 0x1a70, offset32: 0x69c, 
+
+ConfigEffect
+field: id, value: 6, offset: 0x1c22, 
+field: effect_level, value: 3, offset: 0x1c26, 
+field: size, value: 424, offset: 0x1c2a, 
+field: unknown0, value: 0, offset: 0x1c2e, 
+
+ConfigEffect
+field: id, value: 6, offset: 0x1dda, 
+field: effect_level, value: 1, offset: 0x1dde, 
+field: size, value: 412, offset: 0x1de2, 
+field: unknown0, value: 0, offset: 0x1de6, 
+
+ConfigEffect
+field: id, value: 6, offset: 0x1f86, 
+field: effect_level, value: 2, offset: 0x1f8a, 
+field: size, value: 426, offset: 0x1f8e, 
+field: unknown0, value: 0, offset: 0x1f92, 
+
+ConfigEffect
+field: id, value: 7, offset: 0x2140, offset32: 0x850, 
+field: effect_level, value: 3, offset: 0x2144, offset32: 0x851, 
+field: size, value: 409, offset: 0x2148, offset32: 0x852, 
+field: unknown0, value: 0, offset: 0x214c, offset32: 0x853, 
+
+ConfigEffect
+field: id, value: 7, offset: 0x22e9, 
+field: effect_level, value: 1, offset: 0x22ed, 
+field: size, value: 409, offset: 0x22f1, 
+field: unknown0, value: 0, offset: 0x22f5, 
+
+ConfigEffect
+field: id, value: 7, offset: 0x2492, 
+field: effect_level, value: 2, offset: 0x2496, 
+field: size, value: 394, offset: 0x249a, 
+field: unknown0, value: 0, offset: 0x249e, 
+
+ConfigEffect
+field: id, value: 8, offset: 0x262c, offset32: 0x98b, 
+field: effect_level, value: 3, offset: 0x2630, offset32: 0x98c, 
+field: size, value: 424, offset: 0x2634, offset32: 0x98d, 
+field: unknown0, value: 0, offset: 0x2638, offset32: 0x98e, 
+
+ConfigEffect
+field: id, value: 8, offset: 0x27e4, offset32: 0x9f9, 
+field: effect_level, value: 1, offset: 0x27e8, offset32: 0x9fa, 
+field: size, value: 403, offset: 0x27ec, offset32: 0x9fb, 
+field: unknown0, value: 0, offset: 0x27f0, offset32: 0x9fc, 
+
+ConfigEffect
+field: id, value: 8, offset: 0x2987, 
+field: effect_level, value: 2, offset: 0x298b, 
+field: size, value: 418, offset: 0x298f, 
+field: unknown0, value: 0, offset: 0x2993, 
+
+ConfigEffect
+field: id, value: 9, offset: 0x2b39, 
+field: effect_level, value: 3, offset: 0x2b3d, 
+field: size, value: 276, offset: 0x2b41, 
+field: unknown0, value: 0, offset: 0x2b45, 
+
+ConfigEffect
+field: id, value: 9, offset: 0x2c5d, 
+field: effect_level, value: 1, offset: 0x2c61, 
+field: size, value: 252, offset: 0x2c65, 
+field: unknown0, value: 0, offset: 0x2c69, 
+
+ConfigEffect
+field: id, value: 9, offset: 0x2d69, 
+field: effect_level, value: 2, offset: 0x2d6d, 
+field: size, value: 268, offset: 0x2d71, 
+field: unknown0, value: 0, offset: 0x2d75, 
+
+ConfigEffect
+field: id, value: 10, offset: 0x2e85, 
+field: effect_level, value: 3, offset: 0x2e89, 
+field: size, value: 276, offset: 0x2e8d, 
+field: unknown0, value: 0, offset: 0x2e91, 
+
+ConfigEffect
+field: id, value: 10, offset: 0x2fa9, 
+field: effect_level, value: 1, offset: 0x2fad, 
+field: size, value: 252, offset: 0x2fb1, 
+field: unknown0, value: 0, offset: 0x2fb5, 
+
+ConfigEffect
+field: id, value: 10, offset: 0x30b5, 
+field: effect_level, value: 2, offset: 0x30b9, 
+field: size, value: 268, offset: 0x30bd, 
+field: unknown0, value: 0, offset: 0x30c1, 
+
+ConfigEffect
+field: id, value: 11, offset: 0x31d1, 
+field: effect_level, value: 3, offset: 0x31d5, 
+field: size, value: 276, offset: 0x31d9, 
+field: unknown0, value: 0, offset: 0x31dd, 
+
+ConfigEffect
+field: id, value: 11, offset: 0x32f5, 
+field: effect_level, value: 1, offset: 0x32f9, 
+field: size, value: 252, offset: 0x32fd, 
+field: unknown0, value: 0, offset: 0x3301, 
+
+ConfigEffect
+field: id, value: 11, offset: 0x3401, 
+field: effect_level, value: 2, offset: 0x3405, 
+field: size, value: 268, offset: 0x3409, 
+field: unknown0, value: 0, offset: 0x340d, 
+
+Remove effect 1_2, duplicate of 1_1
+Remove effect 1_3, duplicate of 1_1
+Remove effect 1_4, duplicate of 1_1
+Remove effect 1_9, duplicate of 1_8
+Remove effect 1_10, duplicate of 1_8
